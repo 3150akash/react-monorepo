cfef02e6a4f25134124f7af66aa3775251fdefb1
Akash Rawat <3150akash@gmail.com>
2024-07-27 22:44:45 +0530

    conclusion

--- libs/products/src/lib/products.tsx
+++ libs/products/src/lib/products.tsx
@@ -1,9 +1,10 @@
 import styles from './products.module.css';
-
+import { Orders } from '@react-monorepo/orders';
 export function Products() {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Products!</h1>
+      <p>This is a change. 👋</p>
     </div>
   );
 }
